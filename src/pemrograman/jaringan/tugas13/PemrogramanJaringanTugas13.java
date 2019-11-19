5d4a19b43cbc20fd87c212479c6cc081d8d2ee3d
od3ng <noprianto@polinema.ac.id>
2019-11-19 10:54:24 +0700

    commit pertama

---
+++ src/pemrograman/jaringan/tugas13/PemrogramanJaringanTugas13.java
@@ -0,0 +1,27 @@
+/*
+ * pemrograman-jaringan-tugas13
+
+ * Copyright (c) 2019
+ * All rights reserved.
+ * Written by od3ng created on Nov 19, 2019 10:08:35 AM
+ * Blog    : sinaungoding.com
+ * Email   : lepengdados@gmail.com
+ * Github  : 0d3ng
+ * Hp      : 085878554150
+ */
+package pemrograman.jaringan.tugas13;
+
+/**
+ *
+ * @author od3ng
+ */
+public class PemrogramanJaringanTugas13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
